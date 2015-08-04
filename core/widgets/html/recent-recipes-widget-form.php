3b2a82cd57ad3ccac55b5c50dc681922b1919fc2
lchelak <lchelak@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-04 21:07:05 +0000

    Add new version number 1.3.6 files
    git-svn-id: https://plugins.svn.wordpress.org/simmer/tags/1.3.6@1212929 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ core/widgets/html/recent-recipes-widget-form.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Display the Recent Recipes widget admin markup
+ * 
+ * @since 1.1.0
+ * 
+ * @package Simmer\Widgets
+ */
+?>
+
+<p>
+	<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', Simmer()->domain ); ?></label>
+	<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $instance['title'] ); ?>" />
+</p>
+
+<p>
+	<label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of recipes to show:', Simmer()->domain ); ?></label>
+	<input type="number" id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" value="<?php echo esc_attr( $instance['number'] ); ?>" min="0" style="width:50px;"/>
+</p>
+
+<p>
+	<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'show_dates' ); ?>" name="<?php echo $this->get_field_name( 'show_dates' ); ?>" <?php checked( $instance['show_dates'] ); ?> />
+	<label for="<?php echo $this->get_field_id( 'show_dates' ); ?>"><?php _e( 'Display relative dates', Simmer()->domain ); ?></label>
+</p>
